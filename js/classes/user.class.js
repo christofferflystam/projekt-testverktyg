75e10d798e3aa5f74b8c3e4d06cfa771dae56b91
Alexander Aschan <alexander.aschan@live.se>
2017-02-22 11:30:29 +0100

    Created classes for sql queries

--- js/classes/user.class.js
+++ js/classes/user.class.js
@@ -1,14 +1,19 @@
 class User{
 
 /*Add Array for completed tests*/
-constructor(username, firstName, lastName, role){
+constructor(username, password, firstName, lastName, role){
   this.username = username;
+  this.password = password;
   this.firstName = firstName;
   this.lastName = lastName;
   this.role = role;
 }
 get username(){ //getter
   return this.username; 
+}
+
+get password(){
+	return this.password;
 }
 
 get name(){ 
@@ -18,5 +23,23 @@ get name(){
 get role(){
 	return this.role;
 }
+
+  insertInDb(callback){
+    this.db.newUser({
+      username: this.username,
+      first_name: this.firstName,
+      last_name: this.lastName,
+      password: this.password,
+      role: this.role
+    },callback);
+  }
+
+  static get sqlQueries(){
+    return {
+      newUser: `
+        INSERT users SET ?
+      ` 
+    }
+  }
 
 }
