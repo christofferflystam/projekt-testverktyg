e68a9a700f4b2257f5102c7a36bdb60c60b413a2
Alexander Aschan <alexander.aschan@live.se>
2017-02-23 21:07:36 +0100

    Added some comments for clarification, also did some formating on the Test template

--- js/classes/app.class.js
+++ js/classes/app.class.js
@@ -2,6 +2,42 @@ class App {
 
   constructor(){
 
+     //<<<<------------------------------------------------------------------->>>>
+
+    //to clarify, every time an object is to be displayed
+    //using a template, it calls the template corresponding
+    //to that object
+
+    //in the below example a TestView object will be generated
+
+    //that TestView object will in turn have a TestList object
+
+    //the TestView class has a template (see the template folder)
+    //that has the line "${this.tests.display()}", this line
+    //basically says "this TestView object will display its 'tests' attribute"
+
+    //the 'tests' attribute in this case is a TestList object
+    //which in turn is a list of Test objects
+
+    //Basically the TestView template can see it should display the
+    //'tests' attribute (which is a TestList object), it then discovers it's a list of Test objects
+    //Test in turn has its own template that tells the browser
+    //how to display a Test object
+
+    //I assume a TestList template is not currently needed since TestList doesn't
+    //have any attributes except for the Test objects so there is nothing
+    //belonging to TestList that needs to be displayed
+
+    //if the Test objects have Question objects (which they don't at the moment)
+    //they in turn are gonna need their own Question template
+
+    //it's also possible to create a list of only Question objects without it
+    //belonging to a Test object, it will still use the same Question template
+
+
+    //<<<<------------------------------------------------------------------->>>>
+
+
     //calls function to load tests from database
     //with a callback to the "start" function
     //which in turn uses the TestView created from the
@@ -13,10 +49,13 @@ class App {
 
   //loads all tests then runs the callback function
   //to display it
+  //in this example the callback keyword will be equal
+  //to the function this.start
   loadTestsFromDb(callback){
     this.callback = callback;
     
     //creates an empty TestList
+    //that will contain the Test objects
     var listFromDb = new TestList();
     
     //populates the empty TestList using
@@ -41,6 +80,8 @@ class App {
     //as an argument in this function, and then
     //applies the newly created TestView as
     //an argument
+    //remember that callback in this case is the 
+    //function we supplied before which was this.start
     this.callback(theTestView);
     
     
@@ -53,13 +94,14 @@ class App {
   //to display everything
   start(testView){
     
-
+    //creates a HeaderFooter object
     this.HeaderFooter = new HeaderFooter();
     
     //sets its own TestView variable to the TestView
     //created back in loadTestsFromDb
     this.testView = testView;
     
+    //logs to see that all data is there
     console.log(testView);
     
     //logs the specific name of the retrieved test
@@ -72,6 +114,8 @@ class App {
     
     //displays the TestView object in the content div
     //that belongs to the HeaderFooter object
+    //this starts the chain of checking templates
+    //to see how it is supposed to be displayed
     this.testView.display('.content');
 
     
