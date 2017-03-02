e249f5d0f737e13f409ed27190d8f51d05e29098
Alexander Aschan <alexander.aschan@live.se>
2017-03-02 13:11:13 +0100

    created function to change active question

--- js/classes/question-sidebar.class.js
+++ js/classes/question-sidebar.class.js
@@ -10,4 +10,16 @@ class QuestionSidebar extends Base {
     super(propertyValues);
   }
 
+
+  changeActive(){
+	  	this.clickedQuestion = (document.getElementById('question_'+this.question_number));
+	  	console.log('which question did I click?', this.clickedQuestion);
+	  	$(function() {
+    		console.log('what is this1', this.clickedQuestion);
+    	}); 
+    	console.log('what is this', this);
+	  	this.clickedQuestion.removeClass('list-group-item');
+	
+  }
+
 }
